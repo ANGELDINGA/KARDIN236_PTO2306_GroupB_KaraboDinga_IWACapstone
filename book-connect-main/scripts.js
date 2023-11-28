2c552c3c5b7a3ff63fd7f96b8c7b7b69cf0f9ce2
Karabo Angel Dinga <skyydinga@gmail.com>
2023-11-28 18:12:40 +0200

    search functionality

--- book-connect-main/scripts.js
+++ book-connect-main/scripts.js
@@ -8,8 +8,8 @@ import { BOOKS_PER_PAGE, authors, genres, books  } from './data.js';
 // data types from html
 const getElement = (selector) => document.querySelector(selector);
 
-const activeList = document.querySelector("[data-list-active]");
-const dataList = document.querySelector("[data-list-items]");
+const activeList = getElement("[data-list-active]");
+const dataList = getElement("[data-list-items]");
 const settingsButton = getElement('[data-header-settings]');
 const settingsOverlay = getElement('[data-settings-overlay]');
 const settingsForm = getElement('[data-settings-form]');
@@ -17,8 +17,7 @@ const settingsTheme = getElement('[data-settings-theme]');
 const settingsCancel = getElement('[data-settings-cancel]');
 const searchForm = getElement('[data-search-form]');
 const searchOverlay = getElement('[data-search-overlay]');
-const bookList1 = getElement('[data-list-items]');
-const messageList = getElement('[data-list-message]');
+const listMessage = getElement('[data-list-message]');
 const buttonList = getElement('[data-list-button]');
 const bookPicture = getElement("[data-list-image]");
 const bookTitle = getElement("[data-list-title]");
@@ -26,11 +25,26 @@ const bookSubtitle =  getElement("[data-list-subtitle]");
 const blurryBookPic = getElement("[data-list-blur]");
 const closeButton = getElement("[data-list-close]")
 const bookDescription = getElement("[data-list-description]")
+const buttonHeader = getElement("[data-header-search]")
+const cancelButton = getElement("[data-search-cancel]")
+const genreSearch = getElement("[data-search-genres]");
+const searchAuthor = getElement("[data-search-authors]");
+const titleSearch = getElement("[data-search-title]");
+const formDivSearch = getElement('[id="search"]');
 
+/**
+ * Event listener for the settings button click.
+ * Opens the settings overlay.
+ */
 settingsButton.addEventListener('click', () => {
     settingsOverlay.showModal();
 });
 
+
+/**
+ * Event listener for the settings cancel button click.
+ * Closes the settings overlay.
+ */
 settingsCancel.addEventListener('click', () => {
     settingsOverlay.close();
 });
@@ -208,6 +222,110 @@ const appendBooks = (booksToAppend) => {
   appendBooks(initialBooks);
   updateShowMoreButton(books.length - BOOKS_PER_PAGE);
   
-  // Event listener for "Show more" button click
+  /**
+ * Event listener for the "Show more" button click.
+ * Handles the addition of more books to the displayed list.
+ */
   buttonList.addEventListener('click', handleShowMoreClick);
+
+  /**
+ * Function to toggle the search overlay and focus on the title input.
+ */
+const toggleSearchOverlay = () => {
+    if (!searchOverlay.open) {
+      searchOverlay.showModal();
+      titleSearch.focus();
+    } else {
+      searchOverlay.close();
+    }
+  };
+  
+  /**
+ * Event listener for the header button and cancel button in the search overlay.
+ * Toggles the search overlay.
+ */
+  buttonHeader.addEventListener("click", toggleSearchOverlay);
+  cancelButton.addEventListener("click", toggleSearchOverlay);
+  
+  // Function to filter books based on search criteria
+  // Function to filter books based on search criteria
+  const filterBooks = (filters) => {
+    return books.filter((book) => {
+      const trimmedTitle = filters.title.trim().toLowerCase();
+      const bookTitleLowerCase = book.title.toLowerCase();
+      const bookTitleMatch = trimmedTitle === "" || bookTitleLowerCase.includes(trimmedTitle);
+
+  
+      
+      const bookAuthorMatch = filters.author === "All Authors" || book.author.includes(filters.author);
+      const bookGenreMatch = filters.genre === "All Genres" || book.genres.includes(filters.genre);
+  
+      console.log(`Book: ${book.title}, Trimmed Title: ${trimmedTitle}, Title Match: ${bookTitleMatch}`);
+      console.log('Author Match:', bookAuthorMatch);
+      console.log('Genre Match:', bookGenreMatch);
+      
+      const isMatch = bookTitleMatch && bookAuthorMatch && bookGenreMatch;
+      console.log('Overall Match:', isMatch);
+  
+      return isMatch;
+    });
+  };
+  
+  
+  
+
+  
+  // Function to update UI based on search results
+  const updateUIWithResults = (results) => {
+    console.log('Results:', results);
+    listMessage.classList.toggle("list__message_show", results.length < 1);
+  
+    const remainingBooks = Math.max(0, results.length - 36);
+    buttonList.disabled = remainingBooks === 0;
+    buttonList.textContent = `Show more (${remainingBooks})`;
+  
+    dataList.replaceChildren(createPreviewFragment(results, 0, 36));
+    
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  
+  // Event listener for form submission
+  searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    searchOverlay.close();
+  
+    const form_data = new FormData(document.getElementById('search'));
+
+    const filters = Object.fromEntries(form_data);
+    console.log('Filters:', filters);
+    const results = filterBooks(filters);
+    updateUIWithResults(results);
+  });
+  
+  // Function to create and append options for genres
+  const createGenreOptions = () => {
+    genreSearch.innerHTML = `<option>All Genres</option>`;
+    for (const [id, name] of Object.entries(genres)) {
+      const genreOptions = document.createElement("option");
+      genreOptions.innerText = name;
+      genreOptions.value = id;
+      genreSearch.appendChild(genreOptions);
+    }
+  };
+  
+  // Function to create and append options for authors
+  const createAuthorOptions = () => {
+    searchAuthor.innerHTML = `<option>All Authors</option>`;
+    for (const [id, name] of Object.entries(authors)) {
+      const authorOpt = document.createElement("option");
+      authorOpt.innerText = name;
+      authorOpt.value = id;
+      searchAuthor.appendChild(authorOpt);
+    }
+  };
+  
+  // Initialize genre and author options
+  createGenreOptions();
+  createAuthorOptions();
+  
   
